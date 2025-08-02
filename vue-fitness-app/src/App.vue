bb0163e62b413f2c4f9a026aa721af03e3012114
Hazerin <alby91v@gmail.com>
2025-08-02 18:01:44 +0200

    First checkpoint

---
+++ vue-fitness-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import Welcome from './components/pages/Welcome.vue';
+
+</script>
+
+<template>
+    <h1 class="text-gradient">Hello world</h1>
+    <Welcome/>
+</template>
+
+<style scoped>
+
+</style>
