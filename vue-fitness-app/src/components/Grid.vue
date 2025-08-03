bde981adc40332dda0e856df423a4f6ab17dbebf
Hazerin <alby91v@gmail.com>
2025-08-03 17:49:07 +0200

    pushing

--- vue-fitness-app/src/components/Grid.vue
+++ vue-fitness-app/src/components/Grid.vue
@@ -1,9 +1,22 @@
 <script setup>
+    import { workoutProgram } from '../utils';
+    const workoutTypes = ['Push','Pull','Legs'];
 
 </script>
 
 <template>
-
+    <section id="grid">
+        <!--Coppia chiave-valore (0 : "0", 1 : "1" etc) in modo da dare a ogni pulsante una chiave unica per l'identificazione-->
+        <button :key="workoutIdx" v-for="(workout, workoutIdx) in Object.keys(workoutProgram)" class="card-button plan-card" >
+            <div>
+                <p>Day {{ workoutIdx < 9 ? "0" + (workoutIdx + 1) : workoutIdx + 1}}</p>
+                <i class="fa-solid fa-dumbbell"></i>
+                <i class="fa-solid fa-weight-hanging"></i>
+                <i class="fa-solid fa-bolt"></i>
+            </div>
+            <h3>ITEMS</h3>
+        </button>
+    </section>
 </template>
 
 <style scoped>
--- vue-fitness-app/src/components/pages/Dashboard.vue
+++ vue-fitness-app/src/components/pages/Dashboard.vue
@@ -1,5 +1,10 @@
 <script setup>
+  import { gymHealthFacts } from '../../utils';
+  import Grid from '../Grid.vue'
 
+  const randomNumber = Math.floor(Math.random() * gymHealthFacts.length)
+  const todayFact = gymHealthFacts[randomNumber]
+  
 </script>
 
 <template>
@@ -7,13 +12,39 @@
     <div class="card tip-container">
       <h2>Welcome Smoldiers</h2>
       <div>
-        <p class="tip"><strong>Daily Tip</strong><br/>INJECT THE TIP</p>
+        <p class="tip"><strong>Daily Tip</strong><br/>{{ todayFact }}</p>
       </div>
+      <button>Start workout &rarr;</button>
     </div>
+    <Grid/>
 
   </section>
 </template>
 
 <style scoped>
+  .tip-container,
+  .tip-container div,
+  #dashboard {
+    display: flex;
+  }
+
+  .tip-container,
+  #dashboard {
+    flex-direction: column;
+  }
+
+  #dashboard {
+    gap: 2rem;
+  }
+
+  .tip-container {
+    gap: 0.5rem;
+  }
+
+  @media (min-width: 640px) {
+    .tip-container {
+      gap: 1rem;
+    }
+  }
 
 </style>
